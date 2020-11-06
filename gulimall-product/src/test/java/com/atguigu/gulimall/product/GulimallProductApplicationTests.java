30735b51fd2b36facd0a981c7945aecba071db1c
rhf <1028623847@qq.com>
2020-11-06 17:21:38 +0800

    引入人人逆向工程并完成商品服务的基本搭建

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,25 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
+    @Autowired
+    private BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandEntity.setLogo("荷花");
+        brandEntity.setDescript("华为5G");
+        brandEntity.setShowStatus(1);
+        brandEntity.setFirstLetter("H");
+        brandService.save(brandEntity);
     }
 
 }
