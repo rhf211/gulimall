3e6e55108f17c0be70898be9447f611c43c444d7
rhf <1028623847@qq.com>
2021-01-08 16:57:39 +0800

    修改产品接口

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/AttrService.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/AttrService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.gulimall.product.entity.AttrEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +17,11 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    List<AttrEntity> getRelationAttr(Long attrgroupId);
+
+    PageUtils getNoRelationAttr(Map<String, Object> params, Long attrgroupId);
+
+    PageUtils queryBaseAttrPage(Map<String, Object> params, Long catelogId, String type);
 }
 
